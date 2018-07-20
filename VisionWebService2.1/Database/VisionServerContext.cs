f594b8a287531412344df07efe6b8e424b7df1a2
Christopher Klumb <christopher.klumb@hotmail.de>
2018-07-20 23:08:51 +0200

    copied all files

---
+++ VisionWebService2.1/Database/VisionServerContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VisionLib.Database;
+using VisionLib.Models;
+
+namespace VisionWebService.Database
+{
+    public class VisionServerContext : VisionDbContext
+    {
+        public DbSet<User> Users { get; private set; }
+
+        public DbSet<Artist> Artists { get; private set; }
+
+        public DbSet<Track> Tracks { get; private set; }
+
+        public DbSet<Album> Albums { get; private set; }
+
+        public DbSet<Repository> Repositories { get; private set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlite("Data Source=music.db");
+        }
+    }
+}
--- VisionWebService2.1/Startup.cs
+++ VisionWebService2.1/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using VisionWebService.Database;
 
 namespace VisionWebService2._1
 {
@@ -26,6 +27,7 @@ namespace VisionWebService2._1
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddDbContext<VisionServerContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
